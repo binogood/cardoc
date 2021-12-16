08157afca2a21a9c47e017091af8c03dd75057cd
binogood <binogood68@gmail.com>
2021-12-17 01:19:10 +0900

    Update: Foreignkey table name 변경

--- app/user/models/user_car.py
+++ app/user/models/user_car.py
@@ -9,8 +9,8 @@ class UserCar(Base):
     __tablename__ = "user_cars"
 
     user_car_id = Column(Integer, primary_key=True, autoincrement=True)
-    car_id = Column(Integer, ForeignKey('car.car_id'), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.user_id'), nullable=False)
+    car_id = Column(Integer, ForeignKey('cars.car_id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
 
     def create(self, car_id: int, user_id: int) -> Union["UserCar", NoReturn]:
         return UserCar(car_id=car_id, user_id=user_id)
