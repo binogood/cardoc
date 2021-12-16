63b3f20a9c24c96988b1c61b972bc63a72c3e454
binogood <binogood68@gmail.com>
2021-12-17 01:08:24 +0900

    Update: dependencies 추가 - Dependencies추가로 admin계정만 api사용 가능 변경

--- app/car/views/car_tire.py
+++ app/car/views/car_tire.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Header
-from typing import Optional
+from fastapi import APIRouter, Depends
 
 from app.car.request.car_tire import CreateCarTireRequest
 from app.car.response.car_tire import CreateCarTireResponse
 from app.car.service.car_tire import CarTireService
 
 from core.fastapi.schemas.response import ExceptionResponseSchema
+from core.fastapi.dependencies.permission import PermissionDependency, IsAdmin
 
 car_tire_router = APIRouter()
 
@@ -14,7 +14,8 @@ car_tire_router = APIRouter()
     "/create-car-tire",
     response_model=CreateCarTireResponse,
     responses={"400": {"model": ExceptionResponseSchema}},
+    dependencies=[Depends(PermissionDependency([IsAdmin]))],
     summary="Create Car",
 )
-async def create_car_tire(request: CreateCarTireRequest, token: Optional[str] = Header(None)):
-    return await CarTireService().create_car_tire(token, **request.dict())
+async def create_car_tire(request: CreateCarTireRequest):
+    return await CarTireService().create_car_tire(**request.dict())
