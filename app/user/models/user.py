54230dde63ff36967f323adf79ccfd17d15fabb9
binogood <binogood68@gmail.com>
2021-12-17 01:15:56 +0900

    Update: is_admin추가 - admin로직 추가로 인하여 is_admin추가

--- app/user/models/user.py
+++ app/user/models/user.py
@@ -1,6 +1,6 @@
 from typing import Union, NoReturn
 
-from sqlalchemy import Column, Integer, Unicode, DateTime, func
+from sqlalchemy import Column, Integer, Unicode, DateTime, func, Boolean
 
 from core.db.session import Base
 from core.db.timestamp_mixin import TimestampMixin
@@ -13,6 +13,7 @@ class User(Base, TimestampMixin):
     user_id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(Integer, nullable=False, unique=True)
     password = Column(Unicode(255), nullable=False)
+    is_admin = Column(Boolean, default=False)
     created_at = Column(
                         DateTime,
                         nullable=False,
@@ -26,9 +27,14 @@ class User(Base, TimestampMixin):
                         onupdate=func.utc_timestamp(),
                 )
 
-    def create(self, name: str, password: str) -> Union["User", NoReturn]:
+    def create(
+            self,
+            name: str,
+            password: str,
+            is_admin: bool,
+    ) -> Union["User", NoReturn]:
         password = self._hashing_password(password=password)
-        return User(name=name, password=password)
+        return User(name=name, password=password, is_admin=is_admin)
 
     def _hashing_password(self, password: str) -> str:
         return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
