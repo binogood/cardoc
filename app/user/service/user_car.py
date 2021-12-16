a37d91bf52377103f05129fef62984e727c3ddb6
binogood <binogood68@gmail.com>
2021-12-17 01:19:34 +0900

    Update: Request추가 - Request 추가

--- app/user/service/user_car.py
+++ app/user/service/user_car.py
@@ -1,6 +1,5 @@
-
 from typing import Union, NoReturn
-
+from fastapi import Request
 from pythondi import inject
 
 from app.user.models.user_car import UserCar
@@ -10,7 +9,6 @@ from app.car.repository.car_tire import CarTireRepo
 from app.user.repository.user_car import UserCarRepo
 from core.db.transaction import Transaction, Propagation
 from core.exceptions.user import (
-    UserNotFoundException,
     DuplicateUserCarException,
     ManyRequestsException,
 )
@@ -35,43 +33,39 @@ class UserCarService:
     @Transaction(propagation=Propagation.REQUIRED)
     async def create_user_car(
                     self,
+                    user_info: Request,
                     user_car_info: list,
                 ) -> Union[UserCar, NoReturn]:
+        user_id = user_info.user.id
         if len(user_car_info) > 5:
             raise ManyRequestsException
         user_car_list = []
         for value in user_car_info:
             value = value.dict()
-            key = await self._check_value(value)
+            car_id = await self._check_value(value, user_id)
 
-            user_car = UserCar().create(car_id=key['car_id'], user_id=key['user_id'])
+            user_car = UserCar().create(car_id=car_id, user_id=user_id)
             user_car = await self.user_car_repo.save(user_car=user_car)
             user_car_list.append(user_car)
 
         return user_car_list
 
     @Transaction(propagation=Propagation.REQUIRED)
-    async def get_user_car(self, trimId: int, name: str) -> Union[UserCar, NoReturn]:
-        return await self._user_car_info(trimId=trimId, name=name)
+    async def get_user_car(self, trim_id: int, name: str) -> Union[UserCar, NoReturn]:
+        return await self._user_car_info(trim_id=trim_id, name=name)
 
-
-    async def _user_car_info(self, trimId: int, name: str) -> dict:
-        user = await self.user_repo.get_by_user(name=name)
-        car = await self.car_repo.get_by_car_trim(trimId=trimId)
+    async def _user_car_info(self, trim_id: int, name: str) -> dict:
+        user = await self.user_repo.get_by_name(name=name)
+        car = await self.car_repo.get_by_car_trim(trim_id=trim_id)
         car_tire = await self.car_tire_repo.get_by_car_tire_info(car_id=car.car_id)
-        car_tire_info = {'id': user.name, "trimId": car.trimId, "car_tire": car_tire}
-        print(car_tire_info)
+        car_tire_info = {'id': user.name, "trim_id": car.trim_id, "car_tire": car_tire}
         return car_tire_info
 
-    async def _check_value(self, value: dict) -> dict:
-        user = await self.user_repo.get_by_user(name=value['name'])
-        if not user:
-            raise UserNotFoundException
-        car = await self.car_repo.get_by_car_trim(trimId=value['trimId'])
+    async def _check_value(self, value: dict, user_id: int) -> int:
+        car = await self.car_repo.get_by_car_trim(trim_id=value['trim_id'])
         if not car:
             raise CarNotFoundException
-        if await self.user_car_repo.get_by_user_car(trimId=value['trimId'], name=value['name']):
+        if await self.user_car_repo.get_by_user_car(car_id=car.car_id, user_id=user_id):
             raise DuplicateUserCarException
 
-        user_car_dict = {'user_id': user.user_id, 'car_id': car.car_id}
-        return user_car_dict
+        return car.car_id
