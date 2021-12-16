88a2e1305a0aff1f1cd7da05894fe7ffcebec8d3
binogood <binogood68@gmail.com>
2021-12-17 01:07:04 +0900

    Update: DuplicateCarTireException 주석 해제

--- app/car/service/car_tire.py
+++ app/car/service/car_tire.py
@@ -5,9 +5,9 @@ from app.car.repository.car_tire import CarTireRepo
 from app.car.repository.car import CarRepo
 from app.car.models.car_tire import CarTire
 from core.db.transaction import Transaction, Propagation
-from core.utiles.token_helper import TokenHelper
 from core.exceptions.car import (
     CarNotFoundException,
+    DuplicateCarTireException,
 )
 from core.exceptions.tire import (
     TireNotFoundException
@@ -22,18 +22,17 @@ class CarTireService:
         self.car_repo = car_repo
 
     @Transaction(propagation=Propagation.REQUIRED)
-    async def create_car_tire(self, token: str, car_id: int, tire_id: int):
-        await self._check_value(token=token, car_id=car_id, tire_id=tire_id)
+    async def create_car_tire(self, car_id: int, tire_id: int):
+        await self._check_value(car_id=car_id, tire_id=tire_id)
 
         car_tire = CarTire().create(car_id=car_id, tire_id=tire_id)
         car_tire = await self.car_tire_repo.save(car_tire=car_tire)
         return car_tire
 
-    async def _check_value(self, token:str, car_id: int, tire_id: int):
-        TokenHelper.decode(token)
+    async def _check_value(self, car_id: int, tire_id: int):
         if await self.tire_repo.get_by_tire_id(tire_id=tire_id):
             raise TireNotFoundException
         if await self.car_repo.get_by_car_id(car_id=car_id):
             raise CarNotFoundException
-        # if await self.car_tire_repo.get_by_car_tire(car_id=car_id, tire_id=tire_id):
-        #     raise DuplicateCarTireException
+        if await self.car_tire_repo.get_by_car_tire(car_id=car_id, tire_id=tire_id):
+            raise DuplicateCarTireException
