cde955ca9f08ed6dd3b97946565063feb4cfd1d1
binogood <binogood68@gmail.com>
2021-12-17 01:20:20 +0900

    Update: dependencies 유저 확인 로직 추가 - dependencies 유저 확인 로직 추가

--- app/user/views/user_car.py
+++ app/user/views/user_car.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request
 
 from typing import List, Optional
 
@@ -8,7 +8,9 @@ from app.user.request.user_car import (
 from app.user.response.user_car import CreateUserCarResponse, GetUserCarResponse
 from app.user.service.user_car import UserCarService
 
+from core.fastapi.dependencies.permission import PermissionDependency, IsAuthenticated
 from core.fastapi.schemas.response import ExceptionResponseSchema
+
 
 user_car_router = APIRouter()
 
@@ -17,17 +19,19 @@ user_car_router = APIRouter()
     "/create-user-car",
     response_model=List[CreateUserCarResponse],
     responses={"400": {"model": ExceptionResponseSchema}},
+    dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
     summary="Create User Car",
 )
-async def create_user_car(request: List[CreateUserCarDetailRequest]):
-    return await UserCarService().create_user_car(**request.dict())
+async def create_user_car(request: List[CreateUserCarDetailRequest], user_info: Request):
+    return await UserCarService().create_user_car(user_info, request)
 
 
 @user_car_router.get(
     "/get-user-car",
     response_model=GetUserCarResponse,
     responses={"400": {"model": ExceptionResponseSchema}},
+    dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
     summary="Get User Car",
 )
-async def get_user_car(trimId: Optional[int], name: Optional[str]):
-    return await UserCarService().get_user_car(trimId=trimId, name=name)
+async def get_user_car(trim_id: Optional[int], name: Optional[str]):
+    return await UserCarService().get_user_car(trim_id=trim_id, name=name)
