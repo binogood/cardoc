4b8047ed8766b15fc1912a99602c765ca5857bf9
binogood <binogood68@gmail.com>
2021-12-17 01:15:02 +0900

    Update: token검사 Exception 추 - InvalidTokenException - InvalidTokenSchemeException

--- core/exceptions/token.py
+++ core/exceptions/token.py
@@ -11,3 +11,16 @@ class ExpiredTokenException(CustomException):
     code = 400
     error_code = 10001
     message = "Expired token"
+
+
+class InvalidTokenException(CustomException):
+    code = 400
+    error_code = 10002
+    message = "Invalid token"
+
+
+class InvalidTokenSchemeException(CustomException):
+    code = 400
+    error_code = 10003
+    message = "Invalid token scheme"
+
